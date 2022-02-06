ff79b2111b101759ad38ad64ed7d8645ed18ebd7
soncreux <98056199+soncreux@users.noreply.github.com>
2022-02-06 16:17:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const firstName = 'Dima'
+//const lastName = 'Kanurin'
+const age = 17
+
+alert('Гавс приветствует тебя, ' + prompt('Как вас зовут', ''))
+//const lastName = prompt('Введите фамилию')
+//alert(firstName + ' ' + lastName)
+//let test = prompt("Test", 'fdfd');
